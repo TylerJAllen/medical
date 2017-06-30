5078e172331b5cc7e09500aa2df068127c9fc37d
Tyler Allen <mynameistylerallen@gmail.com>
2017-06-30 16:18:35 -0700

    add .displayDoctors() as a callback function to .getDoctors(), and add if statement in .extractDoctorInfo() to display yes/no instead of true/false

--- js/medical-interface.js
+++ js/medical-interface.js
@@ -1,21 +1,40 @@
 var Issue = require('./../js/medical.js').issueModule;
 
 
-// Issue.prototype.displayDoctors = function(){
-//   var practiceName = this.getPracticeName();
-//
-//   $('#showResults').append('<div class="individualResult"><div class="practiceInfo"><h1>' +
-//   practiceName + '</h1><h2>' +
-//   );
-// }
+Issue.prototype.displayDoctors = function(){
+  var array = this.resultsArray;
+  var length = array.length;
+  console.log(array);
+  for(var i = 0; i < length; i++){
+    console.log("inside loop test - start");
+    var output = array[i];
+    $('#showResults').append('<div class="individualResult"><div class="practiceInfo"><h1>' +
+    output.practiceName + '</h1><h3>' +
+    output.street + '<br>' +
+    output.city + '<br>' +
+    output.state + '<br>' +
+    output.zip + '</h3><h3>Phone: ' +
+    output.phone + '</h3><h3><a href="' +
+    output.website +
+    '">website</a></h3><h4>Accepts new patients: ' +
+    output.newPatients + '</h4><form class="insuranceForm"><label for="insuranceToggle"><input type="checkbox" name="" value=""> Show Insurance Providers</label></form></div><div class="doctorInfo"><img src="' +
+    output.image + '" alt=""><h2>' +
+    output.firstName + ' ' +
+    output.lastName + ', ' +
+    output.title + '</h2><p>' +
+    output.bio + '</p></div></div>'
+    );
+  }
+  $('#showResults').show();
+};
 
 $(function(){
   $("#medicalForm").submit(function(e){
     e.preventDefault();
+    $('#showResults').empty();
     var medicalIssue = $("#medicalInput").val();
     var NewIssue = new Issue(medicalIssue);
-    NewIssue.getDoctors();
-
-
+    NewIssue.getDoctors(function(){NewIssue.displayDoctors();});
+    // NewIssue.displayDoctors();
   });
 });
--- js/medical.js
+++ js/medical.js
@@ -13,6 +13,12 @@ Issue.prototype.extractDoctorInfo = function(input){
     var practices = input[i].practices[0];
     var addresses = practices.visit_address;
     var profiles = input[i].profile;
+    var newPatientsYesOrNo;
+    if(practices.accepts_new_patients){
+      newPatientsYesOrNo = "Yes";
+    } else {
+      newPatientsYesOrNo = "No";
+    }
     var individualPractice = {
       practiceName: practices.name,
       street: addresses.street,
@@ -21,7 +27,7 @@ Issue.prototype.extractDoctorInfo = function(input){
       zip: addresses.zip,
       phone: practices.phones[0].number,
       website: practices.website,
-      newPatients: practices.accepts_new_patients,
+      newPatients: newPatientsYesOrNo,
       firstName: profiles.first_name,
       lastName: profiles.last_name,
       title: profiles.title,
@@ -29,15 +35,16 @@ Issue.prototype.extractDoctorInfo = function(input){
       bio: profiles.bio,
     };
     this.resultsArray.push(individualPractice);
-  };
+  }
 };
 
 //Get search results from BetterDoctor API and stores in Issue.resultsArray
-Issue.prototype.getDoctors = function(){
+Issue.prototype.getDoctors = function(callback){
   var that = this;
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ that.userInput +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result){
      that.extractDoctorInfo(result.data);
+     callback();
      console.log(that.resultsArray);
     })
    .fail(function(error){
